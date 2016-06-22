edddf80c4df756769433757721a21d0a8d3d5da5
Adam Gyulavari <adam.gyulavari@lab.coop>
2016-06-22 17:22:04 +0200

    modify omniauth config to use secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['spotify_client_id'], ENV['spotify_client_secret'], scope: 'playlist-read-private user-read-private user-read-email user-top-read user-library-read playlist-modify-public'
+  provider :spotify, Rails.application.secrets.spotify_client_id, Rails.application.secrets.spotify_client_secret, scope: 'playlist-read-private user-read-private user-read-email user-top-read user-library-read playlist-modify-public'
 end
