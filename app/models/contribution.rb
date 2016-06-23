be869bedfab86d382546ea272b064369c430ddb4
Adam Gyulavari <adam.gyulavari@lab.coop>
2016-06-24 00:25:17 +0200

    add contribution logic

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,10 +4,15 @@ class Contribution < ActiveRecord::Base
   has_many :tracks
 
   after_create :set_tracks
+  after_create :follow_playlist
 
   def set_tracks
     user.spotify_user.top_tracks(time_range: 'short_term').each do |track|
       tracks.create(spotify_id: track.id)
     end
+  end
+
+  def follow_playlist
+    user.spotify_user.follow!(mixtape.spotify_playlist)
   end
 end
