b03ff204ac636e214c05fc49037282744862c846
Adam Gyulavari <adam.gyulavari@lab.coop>
2016-06-24 02:27:17 +0200

    hotfix: contribute

--- app/controllers/mixtapes_controller.rb
+++ app/controllers/mixtapes_controller.rb
@@ -26,6 +26,7 @@ class MixtapesController < ApplicationController
   def contribute
     mixtape = Mixtape.friendly.find(params[:id])
     mixtape.contributions.create(user: current_user) unless current_user.contributes?(mixtape)
+    mixtape.save
     redirect_to mixtape
   end
 
